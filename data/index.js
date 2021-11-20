f49224f608abf61d0dc46e319ba3a021bd6fada0
rgundam-stevens <93654613+rgundam-stevens@users.noreply.github.com>
2021-11-20 00:02:32 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
 const venueData = require("./venue");
 const userData = require("./user");
+const reviewData = require("./reviews");
 
 module.exports = {
   venues: venueData,
   user: userData,
+  reviews: reviewData,
 };
