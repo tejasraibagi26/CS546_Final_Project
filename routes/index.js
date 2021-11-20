ef0d8f3518194e53ac3d44afd0bc3753ec4c87eb
rgundam-stevens <93654613+rgundam-stevens@users.noreply.github.com>
2021-11-19 22:59:56 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const venues = require("./venues");
 const user = require("./user");
+const reviews = require("./reviews");
 
 const constructorMethod = (app) => {
   app.use("/venues", venues);
   app.use("/user", user);
+  app.use("/reviews", reviews);
   app.use("*", (req, res) => {
     res.status(404).json({ err: "Route not found" });
   });
