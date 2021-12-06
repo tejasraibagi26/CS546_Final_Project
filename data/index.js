86444f5c0e0c3f0af0491e28b635ec26d7b4e20c
Pinak08 <31927067+Pinak08@users.noreply.github.com>
2021-12-06 13:16:46 -0500

    updated index.js in data

--- data/index.js
+++ data/index.js
@@ -1,9 +1,14 @@
 const venueData = require("./venue");
 const userData = require("./user");
+const reviewData = require("./reviews");
+const commentData = require("./comments");
 const request = require("./request");
-
+const admin = require("./admin");
 module.exports = {
   venues: venueData,
   user: userData,
+  reviews: reviewData,
+  comments: commentData,
   request: request,
+  admin: admin,
 };
