e6c0f48c30cfeeba8801f0324e26291b6dfeb602
Pinak08 <31927067+Pinak08@users.noreply.github.com>
2021-12-06 13:27:34 -0500

    updated index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const venues = require("./venues");
 const user = require("./user");
+const reviews = require("./reviews");
+const comments = require("./comments");
 const landing = require("./landing");
 const gameReq = require("./request");
 const create = require("./create");
+const admin = require("./admin");
+const notFound = require("./404");
+const feed = require("./feed");
+const booking = require("./bookings");
 
 const constructorMethod = (app) => {
   app.use("/", landing);
@@ -10,8 +16,14 @@ const constructorMethod = (app) => {
   app.use("/create", create);
   app.use("/post", gameReq);
   app.use("/user", user);
+  app.use("/reviews", reviews);
+  app.use("/comments",comments);
+  app.use("/admin", admin);
+  app.use("/404", notFound);
+  app.use("/feed", feed);
+  app.use("/bookings", booking);
   app.use("*", (req, res) => {
-    res.status(404).redirect("/");
+    res.status(404).redirect("/404");
   });
 };
 
