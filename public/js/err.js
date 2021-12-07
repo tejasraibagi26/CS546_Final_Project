15eab9fb61b8ba7b99dd073ed06d838dd341b9da
Tejas <60872437+tejasraibagi26@users.noreply.github.com>
2021-12-06 23:35:34 -0500

    Fixed err div null error

--- public/js/err.js
+++ public/js/err.js
@@ -1,10 +1,11 @@
 const errDiv = document.getElementById("err");
 
-errDiv.addEventListener("click", function () {
-  console.log("errDiv clicked");
-  errDiv.classList.remove("float-in");
-  errDiv.classList.add("float-out");
-  setTimeout(() => {
-    errDiv.style.display = "none";
-  }, 400);
-});
+if (errDiv) {
+  errDiv.addEventListener("click", function () {
+    errDiv.classList.remove("float-in");
+    errDiv.classList.add("float-out");
+    setTimeout(() => {
+      errDiv.style.display = "none";
+    }, 400);
+  });
+}
